10ca4033608c538127d76c33692b5d6c9ae8279f
Jenkenney-3422 <jenkenney5@gmail.com>
2026-05-13 12:19:00 +0000

    delete tasks_appapp name

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'tasks', # The app you just created
+    'tasks_app', # The app you just created
 ]
 
 # --- DATABASE CONFIG (Postgres) ---

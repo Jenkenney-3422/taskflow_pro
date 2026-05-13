0a500e60570b3f445d29b9ead28f52cf1363070b
Jenkenney-3422 <jenkenney5@gmail.com>
2026-05-13 12:31:41 +0000

    celery.py updation

--- core/celery.py
+++ core/celery.py
@@ -1,13 +1,22 @@
 import os
 from celery import Celery
 
-# Set the default Django settings module
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('taskflow_pro')
+app = Celery('core')
 
-# Read config from Django settings using the 'CELERY' namespace
+# Get the broker URL from environment variables
+broker_url = os.environ.get('CELERY_BROKER_URL', '')
+
+# Logic to handle Render's secure Valkey/Redis connection
+if broker_url.startswith('rediss://'):
+    app.conf.update(
+        broker_use_ssl={'ssl_cert_reqs': 'none'},
+        redis_backend_use_ssl={'ssl_cert_reqs': 'none'},
+    )
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Automatically find tasks.py in your apps
-app.autodiscover_tasks()
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(['tasks_app'])
